06076b6b4100765943c4d92b7db2107148a267e6
Alex Weisberger <alex@relaypayments.com>
2022-07-30 14:36:58 -0400

    Switch to model based test

--- funcorrect.ts
+++ funcorrect.ts
@@ -5,25 +5,52 @@ import { assertEquals } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 
 import fc from 'https://cdn.skypack.dev/fast-check';
 
+class AddRecurringTransactionCommand implements fc.Command<Budget, Client> {
+  constructor(readonly crt: CreateRecurringTransaction) {}
+  check = (m: Readonly<Budget>) => true;
+  async run(b: Budget, c: Client): Promise<void> {
+    b.addRecurringTransaction(this.crt);
+    await c.addRecurringTransaction(this.crt);
+  }
+  toString = () => `addRecurringTransaction(${this.crt})`;
+}
+
 Deno.test("functional correctness", async (t) => {
   let client = new Client();
+  const allCommands = [
+    fc.record({ name: fc.string(), amount: fc.integer() }).map(crt => new AddRecurringTransactionCommand({ ...crt, recurrenceRule: { recurrenceType: "monthly", day: 2 } }))
+  ];
 
-  await fc.assert(fc.asyncProperty(fc.string(), async (text: string) => {
-    let model = new Budget();
-    client = new Client();
+  await fc.assert(
+    fc.asyncProperty(fc.commands(allCommands), async (cmds) => {
+      let model = new Budget();
+      client = new Client();
+
+      const env = () => ({ model, real: client });
+      return fc.asyncModelRun(env, cmds);
+    }).beforeEach(() => {
+      return client.setup()
+    }).afterEach(() => {
+      return client.teardown();
+    })
+  );
+
+  // await fc.assert(fc.asyncProperty(fc.string(), async (text: string) => {
+  //   let model = new Budget();
+  //   client = new Client();
     
-    let rt: CreateRecurringTransaction = { name: "idk", amount: 15.0, recurrenceRule: { recurrenceType: "monthly", day: 2 } };
+  //   let rt: CreateRecurringTransaction = { name: "idk", amount: 15.0, recurrenceRule: { recurrenceType: "monthly", day: 2 } };
 
-    model.addRecurringTransaction(rt);
-    model.viewRecurringTransactions()
+  //   model.addRecurringTransaction(rt);
+  //   model.viewRecurringTransactions()
 
-    await client.addRecurringTransaction(rt);
-    await client.viewRecurringTransactions()
+  //   await client.addRecurringTransaction(rt);
+  //   await client.viewRecurringTransactions()
 
-    return true;
-  }).beforeEach(() => {
-    return client.setup()
-  }).afterEach(() => {
-    return client.teardown();
-  }));
+  //   return true;
+  // }).beforeEach(() => {
+  //   return client.setup()
+  // }).afterEach(() => {
+  //   return client.teardown();
+  // }));
 });
