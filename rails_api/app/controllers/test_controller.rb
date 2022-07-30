c0df09343993856720513ad8bd773c11bc4a49d8
Alex Weisberger <alex@relaypayments.com>
2022-07-30 14:37:11 -0400

    indentation

--- rails_api/app/controllers/test_controller.rb
+++ rails_api/app/controllers/test_controller.rb
@@ -1,12 +1,13 @@
-class TestController < ApplicationController
-    def setup
-        puts 'Test#setup'
-        DatabaseCleaner.strategy = :truncation
-        DatabaseCleaner.start
-    end
 
-    def teardown
-        puts 'Test#teardown'
-        DatabaseCleaner.clean
-    end
+class TestController < ApplicationController
+  def setup
+    puts 'Test#setup'
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    puts 'Test#teardown'
+    DatabaseCleaner.clean
+  end
 end
