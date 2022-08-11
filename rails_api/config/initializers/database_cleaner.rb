8c37f5b1d99ca49e9d795e50de3614e4de96c850
Alex Weisberger <alex@relaypayments.com>
2022-08-11 09:11:48 -0400

    Get tests working with Postgres

---
+++ rails_api/config/initializers/database_cleaner.rb
@@ -0,0 +1,3 @@
+require 'database_cleaner-active_record'
+
+DatabaseCleaner.allow_remote_database_url = true
--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_27_013642) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recurring_transactions", force: :cascade do |t|
     t.string "name"
     t.string "recurrence_rule"
